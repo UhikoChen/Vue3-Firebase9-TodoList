f845ee614a1ea6494d963eb718fe9df35061923b
UhikoChen <chen20825172@gmail.com>
2023-05-29 17:15:25 +0800

    fix: 修正github pages頁面路徑調整

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vue3-Firebase9-Tutorial',
+  base: '/Vue3-Firebase9-TodoList',
   plugins: [vue()],
   resolve: {
     alias: {
